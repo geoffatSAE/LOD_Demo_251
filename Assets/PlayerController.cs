c587f9d0e5dfefb1945e51334d2c423e79ee2191
Geoff Hill @ SAE Institute <g.hill@sae.edu>
2025-03-23 17:05:15 +0000

    Updated player controller with Space

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,8 +1,14 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed, rotateSpeed;
+    int banana;
+
+
+    public GameObject noLODBanana, yesLODBanana;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,5 +25,27 @@ public class PlayerController : MonoBehaviour
         transform.Translate(new Vector3(0, 0, v * Time.deltaTime * moveSpeed));
 
         transform.Rotate(new Vector3(0, h * Time.deltaTime * rotateSpeed, 0));
+
+        if(Input.GetButtonDown("Jump")){
+            
+            if(banana == 0){
+                banana = 1;
+                noLODBanana.SetActive(true);
+                yesLODBanana.SetActive(false);
+            } else if(banana == 1){
+                banana = 2;
+                noLODBanana.SetActive(false);
+                yesLODBanana.SetActive(true);
+            } else {
+                banana = 0;
+                noLODBanana.SetActive(false);
+                yesLODBanana.SetActive(false);
+            }
+
+        }
+
+  
     }
+
+   
 }
